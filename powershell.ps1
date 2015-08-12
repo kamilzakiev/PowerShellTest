0767e38f5a83504bb95fd775466a499c19c969b0
Kamil Zakiev <v-kazaki@microsoft.com>
2015-08-12 11:28:13 -0700

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -2,8 +2,11 @@ $webclient = New-Object System.Net.WebClient
 $url = "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.0.0-windows.zip"
 $file = "phantomjs-2.0.0-windows.zip"
 $webclient.DownloadFile($url,$file)
-dir
+
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::ExtractToDirectory("phantomjs-2.0.0-windows.zip", "extracted")
 copy "extracted/phantomjs-2.0.0-windows/bin/phantomjs.exe" phantomjs.exe
 Get-ChildItem
+
+echo $PSScriptRoot
+Write-Host $PSScriptRoot
